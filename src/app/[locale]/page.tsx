f38756cc79bac102ea907edc1951b012d669d573
Albina Anistratenko <albuska2013@gmail.com>
2024-11-07 11:02:39 +0200

    add multi language in site

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,6 @@
-import { Header } from "@/components/Header";
-
 export default function Home() {
   return (
     <div>
-      <Header />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>Hello world</h1>
       </main>
